963d8caeb7ce535957a40cd537330917cc0e7edc
Felipe da Costa Coqueiro <felipe.coqueiro@usp.br>
2025-06-22 22:07:33 -0300

    Arrumando a documentação

--- frontend-RCEI/src/components/CollaboratorsList.tsx
+++ frontend-RCEI/src/components/CollaboratorsList.tsx
@@ -123,9 +123,6 @@ export function CollaboratorsList() {
           <Users className="h-5 w-5 text-rcei-green-500" />
           Colaboradores
         </h2>
-        <Button className="bg-rcei-green-500 hover:bg-rcei-green-600">
-          Adicionar Colaborador
-        </Button>
       </div>
 
       <div className="border rounded-md">
